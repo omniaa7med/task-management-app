a5f7ada141b3cde7047e481c999ce032a2cc0aa3
omniaa7med <omniaahmed71098@gmail.com>
2025-04-25 01:30:51 +0300

    Add  : handle app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesModule } from './Features/features.module';
+import { TaskState } from './core/state/task.state';
+import { NgxsModule } from '@ngxs/store';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeaturesModule,
+    NgxsModule.forRoot([TaskState]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideHttpClient(withFetch())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

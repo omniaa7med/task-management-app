c6bf62d0a5a7aa962fee79baaf1ebccefc3f7db2
omniaa7med <omniaahmed71098@gmail.com>
2025-04-25 01:31:03 +0300

    add tasklist interface

--- src/app/core/interfaces/task-model.ts
+++ src/app/core/interfaces/task-model.ts
@@ -1,9 +1,18 @@
+import { Observable } from 'rxjs';
+
 export interface Task {
   id: string;
   title: string;
   description?: string;
   dueDate: string;
   status: TaskStatus;
+}
+
+export interface TaskList {
+  title: TaskStatus;
+  connectedDropLists: TaskStatus[];
+  tasks: Observable<Task[]>;
+  titleColor: string;
 }
 
 export enum TaskStatus {

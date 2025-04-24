f166ccf0ee978cb89f79acb07adb9c8eb8233cf1
omniaa7med <omniaahmed71098@gmail.com>
2025-04-25 01:32:04 +0300

    add task card info

---
+++ src/app/Features/tasks/components/task-form/task-form.component.ts
@@ -0,0 +1,57 @@
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Task, TaskStatus } from '../../../../core/interfaces/task-model';
+
+@Component({
+  selector: 'app-task-form',
+  standalone: false,
+  templateUrl: './task-form.component.html',
+  styleUrl: './task-form.component.scss',
+})
+export class TaskFormComponent {
+  @Input() task!: Task | null;
+  @Output() formSubmitted = new EventEmitter<Task>();
+  @Output() cancelled = new EventEmitter<void>();
+  taskStatus = TaskStatus;
+  taskForm!: FormGroup;
+  todayDate: any;
+  constructor(private fb: FormBuilder, private datePipe: DatePipe) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    this.todayDate = this.datePipe.transform(today, 'yyyy-MM-dd');
+  }
+
+  ngOnInit() {
+    const initial: Task | any = this.task || {};
+    this.taskForm = this.fb.group({
+      title: [initial.title || '', [Validators.required]],
+      description: [initial.description || ''],
+      dueDate: [
+        initial.dueDate || this.todayDate,
+        [Validators.required, this.handleErrorPastDate],
+      ],
+      status: [initial.status || TaskStatus.ToDo],
+    });
+  }
+
+  handleErrorPastDate(control: any) {
+    const inputDate = new Date(control.value);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return inputDate < today ? { pastDate: true } : null;
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      const task: Task = {
+        ...this.taskForm.value,
+      };
+
+      this.task?.id ? (task.id = this.task?.id) : null;
+      this.formSubmitted.emit(task);
+    } else {
+      this.taskForm.markAllAsTouched();
+    }
+  }
+}
---
+++ src/app/Features/tasks/components/task-form/task-form.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TaskFormComponent } from './task-form.component';
+
+@NgModule({
+  declarations: [TaskFormComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  providers: [DatePipe],
+  exports: [TaskFormComponent],
+})
+export class TaskFormModule {}

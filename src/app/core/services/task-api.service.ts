8079112ebe8deec504cb3caa77591135b2d901eb
omniaa7med <omniaahmed71098@gmail.com>
2025-04-25 03:50:40 +0300

    Add : json-server

--- src/app/core/services/task-api.service.ts
+++ src/app/core/services/task-api.service.ts
@@ -2,12 +2,15 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '../interfaces/task-model';
+import { status } from './../../../../node_modules/@tinyhttp/send/dist/status';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskApiService {
-  private apiUrl = 'https://6806890be81df7060eb77cdf.mockapi.io/tasks';
+  private apiUrl = 'http://localhost:3000/tasks';
+  // private apiUrl = 'https://6806890be81df7060eb77cdf.mockapi.io/tasks';
+
   private apiKey = 'taskMangment6262';
 
   private get headers() {
@@ -39,7 +42,14 @@ export class TaskApiService {
   updateTaskStatus(id: string, status: string): Observable<any> {
     // we can handle it by PATCH but my mockapi don't allow it to handle PATCH
     // can use it if use json-server db but i use https://mockapi.io/ to try
-    return this.http.put(
+    // return this.http.put(
+    //   `${this.apiUrl}/${id}`,
+    //   { status },
+    //   { headers: this.headers }
+    // );
+
+    // use json-server
+    return this.http.patch(
       `${this.apiUrl}/${id}`,
       { status },
       { headers: this.headers }
